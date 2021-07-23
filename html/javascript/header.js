4eaf1566c57c944a938d6abe000d0f285473c52e
Michael Phelps <dom>
2021-07-23 13:36:10 -0400

    automated from makefile

--- html/javascript/header.js
+++ html/javascript/header.js
@@ -5,74 +5,61 @@ Created:	00/00/2018 - 00:00
 Revised: 	00/00/2018 - 00:00
 -----------------------------------------------------------------------------------------------*/
 var page_title = [
-	"[0] front",
-	"[1] page 1",
-	"[2] page 2",
-	"[3] final",
-	"[4]",
-	"[5]",
-	"[6]",
-	"[7]",
-	"[8]",
-	"[9]",
-	"[10]",
-	"[11]",
-	"[12]",
-	"[13]",
-	"[14]",
-	"[15]",
-	"[16]",
-	"[17]",
-	"[18]",
-	"[19]",
-	"[20]",
+	"[1] Front",
+	"[2] Ambrosia",
+	"[3] Minetest_Dom",
+	"[4] Final",
 ];
 var page_url = [
-	"index",
+	"index.html",
 	"null",
 	"null",
-	"page_final",
-	"null",
-	"null",
-	"null",
-	"null",
-	"null",
-	"null",
-	"null",
-	"null",
-	"null",
-	"null",
-	"null",
-	"null",
-	"null",
-	"null",
-	"null",
-	"null",
-	"null",
+	"final.html",
 ];
-function link_print(x) {
+function generate_navigation(x) {
 	var path = window.location.pathname;
 	var page_current = path.split("/").pop();
-	// Print NULL link.
-	if (page_current = page_url[x] + ".html" || page_url[x] == "null"){
-		document.write("<div id='nav'><p>" + page_title[x] + "</p></div>");
+
+	var loc = window.location.pathname;
+	var dir = loc.substring(0, loc.lastIndexOf('/'));
+
+	if (page_current != page_url[x] && page_url[x] != "null"){
+		// ok
+		document.write("<a href='" + dir + "/" + page_url[x]);
+		document.write("'>");
+		document.write("<div id='nav_active'><p>" + page_title[x] + "</p></div>");
+		document.write("</a>");
 	}
-	// Print OK link.
 	else {
-		document.write("<div id='nav'><a href='" + page_url[x] + ".html'></a></div>");
+		// not ok
+		document.write("<div id='nav_inactive'><p>" + page_title[x] + "</p></div>");
 	};
 };
+	// Print OK link.
+//	if (page_current == page_url[x]){
+//		document.write("<div id='nav'><p>" + page_title[x] + "</p></div>");
+//	}
+	// Print OK link. not current or null
+//	else {
+//		document.write("test");
+//		document.write("<a href=''" + page_url[x] + ".html'");
+//		document.write("<div id='nav'></div>");
+//		document.write("</a>");
+//	};
 //-----------------------------------------------------------------------------------------------
 document.write("<div class='post' id='header'>");
 document.write("<p style='margin-left:0.5em;'>&#9830; Absinthe Interactive Software</p>");
+
 var i;
 for (i = 0; i < page_title.length; i++) {
-	link_print(i);
+	generate_navigation(i);
 };
+
 document.write("</div>");
 
+//----------------------------------------------------
 document.write("<div class='post' id=''>");
-		document.write("<audio controls id='audio' style='width:100%;height:48px;'><source src='html/audio/ada.ogg' type='audio/ogg'>Your browser does not support the audio element.</audio>");
+		document.write("<audio controls id='audio' style='width:100%;height:48px;'><source src='" + var_track + "' type='audio/ogg'>Your browser does not support the audio element.</audio>");
 		var audio = document.getElementById("audio"); audio.volume = 0.2;
 document.write("</div>");
 //-----------------------------------------------------------------------------------------------
