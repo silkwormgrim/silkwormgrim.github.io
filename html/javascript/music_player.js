c5e1375c18111fa0d40fed349a1c051ad786ff14
Dom <87595785+silkwormgrim@users.noreply.github.com>
2021-12-06 13:59:18 -0500

    audio

--- html/javascript/music_player.js
+++ html/javascript/music_player.js
@@ -6,6 +6,8 @@ Revised: 	00/00/2018 - 00:00
 -----------------------------------------------------------------------------------------------*/
 function generate_music_player() {
 
+	document.write("<center><p></br></p></center>");
+
 	document.write("<audio controls autoplay loop id='audio'>");
 	document.write("<source src='html/audio/" + music + ".ogg' type='audio/ogg'>");
 	document.write("<p>Your browser does not support the audio element.</p>");	
