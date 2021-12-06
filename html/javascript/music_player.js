8ed74a484e5cfb4e1b0c8de1aa4d1c8b535c7dd4
Dom <87595785+silkwormgrim@users.noreply.github.com>
2021-12-06 14:54:43 -0500

    more colors

--- html/javascript/music_player.js
+++ html/javascript/music_player.js
@@ -6,14 +6,14 @@ Revised: 	00/00/2018 - 00:00
 -----------------------------------------------------------------------------------------------*/
 function generate_music_player() {
 
-	document.write("<center><p></br></p></center>");
+//	document.write("<center><p></br></p></center>");
 
 	document.write("<audio preload='auto' controls autoplay loop id='audio'>");
 	document.write("<source src='html/audio/" + music + ".ogg' type='audio/ogg'>");
 	document.write("<p>Your browser does not support the audio element.</p>");	
 	document.write("</audio>");
 
-	document.write("<center><p>" + music + "</p></center>");
+//	document.write("<center><p>" + music + "</p></center>");
 
 	var audio = document.getElementById("audio");
 	audio.volume = 0.1;
