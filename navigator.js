25dfbd6ea205e2032eff34771d70d8af29bac3ee
Dom <87595785+silkwormgrim@users.noreply.github.com>
2022-11-17 07:24:41 -0500

    update

--- navigator.js
+++ navigator.js
@@ -6,8 +6,20 @@ var pages = [
 	"home","index.html",
 	"ambrosia","ambrosia.html",
 	"minetest","minetest.html",
-	"final","final.html"
+	"final","final.html",
+/*
+	"et cetera","null",
+	"et cetera","null",
+	"et cetera","null"
+*/
 ];
+
+var navigator  = {
+	on_click() {
+		document.write("testing...");
+	}
+};
+//document.getElementById("navigator").onclick = navigator.onclick();
 
 function generate_navigation() {
 	var symbol = "&#9830; "
@@ -19,22 +31,25 @@ function generate_navigation() {
 
 	var i;
 	for (i = 0; i < pages.length/2; i++) {
-		// print inactive_box
-		if (page_current == pages[i*2+1] || pages[i*2+1] == "null"){
-			document.write("<li>",symbol,pages[i*2],"</li>");
-		}
-		// print active_box
-		else {
-			document.write("<li>","<a href='",dir,"/",pages[i*2+1],"'>");
-			document.write(symbol,pages[i*2]);
-			document.write("</a>","</li>");
-		};
+
+	// print inactive_box
+	if (page_current == pages[i*2+1] || pages[i*2+1] == "null"){
+		document.write("<div class='navigator' id='navigator_inactive'>");
+		document.write(symbol,pages[i*2]);
+		document.write("</div>");
+	}
+	// print active_box
+	else {
+		document.write("<a href='",dir,"/",pages[i*2+1],"'>");
+		document.write("<div class='navigator' id='navigator_active'>");
+		document.write(symbol,pages[i*2]);
+		document.write("</div></a>");
+//		document.getElementById("navigator").onclick = navigator.onclick();
+	};
 	};
 };
 function main_function() {
-	document.write("<ul>");
 	generate_navigation();
-	document.write("</ul>");
 };
 
 main_function();
